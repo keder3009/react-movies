102ba2c86925d746c8584451fba36c90875d2633
k.munoz <k.munoz@superpagos.com>
2023-08-28 19:55:10 -0500

    entendiendo iteraciones con map

---
+++ src/peliculas.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    titulo: "oblivion (2012)",
+    calificacion: "8.1",
+    director: "Joss Whedon",
+    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    fecha: "1 May 2015",
+    duracion: "2h21",
+    img: "mv1.jpg",
+    description:
+      "Earths mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...s",
+  },
+  {
+    titulo: "oblivion (2012)",
+    calificacion: "8.1",
+    director: "Joss Whedon",
+    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    fecha: "1 May 2015",
+    duracion: "2h21",
+    img: "mv1.jpg",
+    description:
+      "Earths mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...s",
+  },
+  {
+    titulo: "oblivion (2012)",
+    calificacion: "8.1",
+    director: "Joss Whedon",
+    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    fecha: "1 May 2015",
+    duracion: "2h21",
+    img: "mv1.jpg",
+    description:
+      "Earths mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...s",
+  },
+];
